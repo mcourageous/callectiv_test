c5c7a43993f7889d82ce85249264021c77793cfb
Courage Mabrey <cmabrey@arrowtesting.com>
2013-10-28 13:12:21 +0000

    added the apikey and secret

--- callectiv.py
+++ callectiv.py
@@ -3,18 +3,27 @@ import unittest
 
 class CallectivTest(unittest.TestCase):
 
-
+	def test_authentication_with_wrong_method(self):
+		""" Send an authentication request with a get method"""
+		
+		auth = {"apikey":"6zUYjnpWPzkPfLmPhwaR","secret":"29xxUQ7ySr"}
+		self.request = requests.post("http://api.dev.callectiv.com",params=auth)
+		self.assertEqual(self.request.status_code,404)
 
 	def test_authentication(self):
+		auth = {"apikey":"6zUYjnpWPzkPfLmPhwaR","secret":"29xxUQ7ySr"}
+		self.request = requests.post("http://api.dev.callectiv.com",params=auth)
+		self.assertEqual(self.request.status_code, 200)
 
-		r = requests.post("http://callectiv.com/authentication")
-		self.assertEqual(r.status_code, 404)
 
-	def test_auth_with_wrong_method(self):
-		r = requests.get("http://callectiv.com/authentication")
-		self.assertEqual(r.status_code,404)
+	# def test_no_authentication(self):
+	# 	"""Send a post request without authentication """
+
+	# 	r = requests.post("http://callectiv.com/authentication")
+	# 	self.assertEqual(r.status_code, 404)
+
+
 	
-
 if __name__ == "__main__":
 	unittest.main()
 
