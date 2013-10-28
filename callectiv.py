a03c175a7de653c9f246cd8a5b958ee3a7bb0689
Courage Mabrey <cmabrey@arrowtesting.com>
2013-10-28 09:40:48 +0000

    authentication with wrong method name

--- callectiv.py
+++ callectiv.py
@@ -3,13 +3,17 @@ import unittest
 
 class CallectivTest(unittest.TestCase):
 
+
+
 	def test_authentication(self):
 
 		r = requests.post("http://callectiv.com/authentication")
 		self.assertEqual(r.status_code, 404)
 
-	def test_auth_method(self):
-		pass
+	def test_auth_with_wrong_method(self):
+		r = requests.get("http://callectiv.com/authentication")
+		self.assertEqual(r.status_code,404)
+	
 
 if __name__ == "__main__":
 	unittest.main()
