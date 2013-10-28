03c27bf405b79941f336647240d05379c5aa9b48
Courage Mabrey <cmabrey@arrowtesting.com>
2013-10-28 17:55:09 +0000

    Subject Details WIP

--- callectiv.py
+++ callectiv.py
@@ -8,6 +8,7 @@ class CallectivTest(unittest.TestCase):
 		self.auth_url = "http://api.callectiv.com/authentication"
 		self.auth =  {"apikey":"6zUYjnpWPzkPfLmPhwaR","secret":"29xxUQ7ySr"}
 		self.headers = {'content-type': 'application/json'}
+		self.subject_url = "http://api.callectiv.com/subject"
 
 
 
@@ -22,7 +23,40 @@ class CallectivTest(unittest.TestCase):
 		self.request = requests.post(self.auth_url, data = json.dumps(self.auth), headers=self.headers)
 		self.assertEqual(self.request.status_code, 200)
 		self.assertEqual(self.request.headers["Content-Type"],"application/xml")
-		self.assertTrue(self.request.headers.get["Content-Type"] =="application/xml")
+		self.assertFalse
+
+
+	# def test_register_subject(self):
+		
+	# 	self.subject_body = {"reference":"12345",
+	# 				"contact":{"phone":"0207508668"},
+	# 				"message":"Callactiv Test"}
+	# 	self.request = requests.post(self.subject_url, data = json.dumps(self.subject_body),headers = self.headers)
+	# 	self.assertIn("12345",self.request.content)
+
+	def test_subject_details(self):
+
+		self.url = "http://api.callectiv.com/subject/12345"
+		self.subject_body = {"reference":"12345",
+					"contact":{"phone":"0207508668"},
+					"message":"Callactiv Test"}
+		self.post = requests.post(self.url, data = json.dumps(self.subject_body),headers = self.headers)
+
+		self.get = requests.get("http://api.callectiv.com/subject/12345")
+		print self.post
+		print self.get
+
+
+		# self.content = self.request.json()
+		# self.assertEqual(self.subject_body['reference'],"12345")
+	
+
+
+
+
+
+
+
 
 
 
