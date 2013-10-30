5116ac7905411f2db5c6a27daf75a1f8c576c5a8
Courage Mabrey <cmabrey@arrowtesting.com>
2013-10-30 13:48:47 +0000

    connection details

--- callectiv.py
+++ callectiv.py
@@ -4,9 +4,15 @@ import json
 import xml.etree.ElementTree 
 import StringIO
 import validictory
+import dateutil.parser as parser
+import datetime
 
 
 class CallectivTest(unittest.TestCase):
+
+	def time(self,text):
+		date = parser.parse(text)
+		return date.isoformat()
 
 	def setUp(self):
 		self.auth_url = "http://api.callectiv.com/authentication"
@@ -50,11 +56,13 @@ class CallectivTest(unittest.TestCase):
 		self.assertEqual(response.get("reference"),"12345")
 		self.assertEqual(response.get("contact")["phone"], "0207508668")
 
-	def test_subject_connections_default_accept_type(self):
+	def test_subject_zonnections_default_accept_type(self):
 		self.connection_url = "http://api.callectiv.com/subject/12345/connections"
 		headers = {"content-type":"application/json","authorization":self.token}
 		self.subject = requests.get(self.connection_url,headers=headers)
+		self.assertEqual(self.subject.status_code, 200)
 		self.assertEqual(xml.etree.ElementTree.fromstring(self.subject.content).tag,"connections")
+		
 	
 
 	def test_subject_connection_with_json_accept_type(self):
@@ -65,8 +73,7 @@ class CallectivTest(unittest.TestCase):
 		expected_schema = {"type":"NoneType"}
 		self.assertIsNot(None,output)
 
-		# self.assertEqual(json.loads(self.subject.content),ValueError)
-		# self.assertEqual(validictory.validate(output,expected_schema))
+	
 
 	def test_change_status_with_get_method(self):
 		"""test status change with wrong method """
@@ -83,7 +90,53 @@ class CallectivTest(unittest.TestCase):
 		self.put = requests.put(self.put_url,headers=headers)
 		self.assertEqual(self.put.status_code, 409)
 		self.assertIsNot(self.put.status_code, 200)
-		print self.put.status_code
+		# print self.put.status_code
+
+	# def test_delete_subject_with_reference(self):
+	# 	""" check status code"""
+	# 	pass
+
+	def test_delete_subject_with_wrong_reference(self):
+		"""check status code"""
+		self.del_url = "http://api.callectiv.com/subject/2347"
+		self.delete = requests.delete(self.del_url,headers=self.headers)
+		self.assertEqual(self.delete.status_code, 401)
+		self.assertFalse(self.delete.status_code == requests.codes.ok)
+
+
+
+	# def test_make_connection(self):
+	# 	"""check status code"""
+	# 	self.make_connection_url = "http://api.callectiv.com/connection"
+	# 	headers = {"content-type":"application/json","authorization":self.token}
+	# 	self.make_connection = requests.post(self.make_connection_url, data= json.dumps(self.connection_request),headers =headers)
+	# 	xml_data = """<connection>
+ # 				   <from>		   
+ #      				  <phone>233207508668</phone>
+ #       				 <message>This is VacationLets. You have a call from the homeowner of the house on Brighton beach.</message>
+ #   						 </from>
+ # 				   <subjectReference>12345</subjectReference>
+ #   					 <startDateTime>2013-10-30T04:29:22+0100</startDateTime>
+	# 				</connection>"""
+
+	# 	print self.make_connection.status_code
+
+
+
+
+		
+
+	# def test_get_connection_details(self):
+	# 	"""check the response body and assert if its equal to what was sent in the post request"""
+	# 	self.detail_url = "http://api.callectiv.com/connection/{connectionId}"
+
+	# 	pass
+
+	# def get_connection_status(self):
+		pass
+
+
+
 
 
 
@@ -98,3 +151,8 @@ if __name__ == "__main__":
 
 
 
+		# self.connection_request ={"from":{
+		# 							"phone":"233207508668",
+		# 							"message":"Hello world"
+		# 							},
+		# 						"subjectReference":"12345","startDateTime": self.time("Wed, 30 Oct 2013 10:50:05 +0000")}
