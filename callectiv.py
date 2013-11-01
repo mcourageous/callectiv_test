e5321336d3bfc75dadbeb4676da7a859b0e2b292
Courage Mabrey <cmabrey@arrowtesting.com>
2013-11-01 15:50:56 +0000

    added tests for make connections]

--- callectiv.py
+++ callectiv.py
@@ -14,14 +14,14 @@ OK = 200
 REFERENCE = "12345"
 REFERENCE_1 = "67890"
 REQUEST_BODY =  {"reference":REFERENCE,
-					"contact":{"phone":"0207508668"},
-					"message":"Callactiv Test"
+				"contact":{"phone":"0207508668"},
+				"message":"Callactiv Test"
 				} 
 
 REQUEST_BODY_1 =  {"reference":REFERENCE,
 					"contact":{"phone":"0207508668"},
 					"message":"Call Test"
-					} 
+				  } 
 
 REQUEST_BODY_XML = """ <subject>
 						<reference>12345</reference>
@@ -32,7 +32,7 @@ REQUEST_BODY_XML = """ <subject>
 						</subject>
 					"""
 
-REQUEST_BODY_XML_1 =  """ <subject>
+REQUEST_BODY_XML_1 =""" <subject>
 						<reference>12345</reference>
 						<contact>
 						<phone> 0207508668</phone>
@@ -40,6 +40,25 @@ REQUEST_BODY_XML_1 =  """ <subject>
 						<message> Call Test</message>
 						</subject>
 					"""
+REQUEST_A = {
+				"to": {
+					"phone":"233207508668",
+					"message": "Please deliver the goods at the mall"
+				},
+				"subjectReference": REFERENCE,
+				"startDateTime":self.MakeConnectionTest.time(str(datetime.datetime.now()))
+	
+				}
+REQUEST_B = 	{
+				"from":{
+						"phone":"233207508668",
+						"message": "This is the place we call home"
+						},
+						"subjectReference":REFERENCE_1,
+						"startDateTime":self.MakeConnectionTest.time(str(datetime.datetime.now()))
+
+						}
+
 
 
 class AuthenticationTest(unittest.TestCase):
@@ -201,7 +220,7 @@ class GetConnectionsSubject(CallectivTestCase):
 		root_xml = etree.fromstring(response.content)
 		self.assertEqual(response.status_code, OK)
 		self.assertEqual(root_xml.tag, 'connections')
-		self.assertIsNotNone(root_xml.find('connection'))
+		self.assertIsNotNone(root_xml.find('connections'))
 		self.assertIsNotNone(root_xml.find('id'))
 		self.assertIsNotNone(root_xml.find('subjectReference'))
 
@@ -228,164 +247,75 @@ class ChangeSubjectStatus(CallectivTestCase):
 		response = requests.put(url, headers=headers)
 		root_xml =etree.fromstring(response.content)
 		self.assertEqual(response.status_code, OK)
-		self.assertIsNotNone(root_xml.find(''))
+		self.assertIsNotNone(root_xml.find('message'))
+		self.assertIsNotNone(root_xml.find('reference'))
+		self.assertIsNotNone(root_xml.find('contact'))
+		self.assertIsNotNone(root_xml.find('creationDateTime'))
 
+	def test_put_method_with_disabled_status(self):
+		url =  'http://api.callectiv.com/subject/12345/status/disabled'
+		headers =  {'Authorization':self.token, 'Accept':APPL_JSON}
+		response = requests.put(url, headers=headers)
+		self.assertEqual(response.status_code, OK)
+
+	def test_put_without_auth(self):
+		url = 'http://api.callectiv.com/subject/12345/status/disabled'
+		headers = {'Accept':APPL_JSON}
+		response = requests.put(url,headers=headers)
+		self.assertNotEqual(response.status_code, OK)
+
+	def test_put_with_disabled_status(self):
+		url = url = 'http://api.callectiv.com/subject/12345/status/disabled'
+		headers =  {'Authorization':self.token, 'Accept':APPL_JSON}
+		response = requests.put(url, headers=headers)
+		content = response.content
+		json_response = json.loads(content)
+		self.assertIsNotNone(json_response)
+		self.assertEqual(json_response.get('status'), 'disabled')
+
+
+class DeleteSubjectTest(CallectivTestCase):
+
+	def test_delete_with_wrong_reference(self):
+		url = 'http://api.callectiv.com/subject/000000'
+		headers = {'Authorization':self.token}
+		response = requests.delete(url, headers=headers)
+		self.assertNotEqual(response.status_code, OK)
+
+	def test_delete_with_reference(self):
+		url = 'http://api.callectiv.com/subject/12345'
+		headers = {'Authorization':self.token}	
+		response = requests.delete(url, headers=headers)
+		self.assertEqual(response.status_code, OK)
+
+	def test_delete_without_auth(self):
+		url = 'http://api.callectiv.com/subject/1234'
+		response = requests.delete(url)
+		self.assertNotEqual(response.status_code, OK)
+
+	def test_delete_without_subject(self):
+		url =  'http://api.callectiv.com/subject/1234'
+		headers =  {'Authorization':self.token}	
+		response = requests.delete(url, headers=headers)
+		self.assertNotEqual(response.status_code, OK)
+
+
+class MakeConnectionTest(CallectivTestCase):
+
+	def test_connection(self):
+		url = 'http://api.callectiv.com/connection'
+		headers = {'Content-Type':APPL_JSON, 'Authorization':self.token}
+		response = requests.post(url, headers=headers, data=json.loads(REQUEST_A))
+		self.assertEqual(response.status_code, OK)
 		
 
 
-
-
-
-
-	
-
-
-	
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
-
-		# headers = {'Content-Type':APPL_JSON, 'Authorization':self.token}
-		# request = requests.post(url, data=json.dumps(request_body), headers=headers)
-		# self.assertEqual(request.status_code, OK)
-		# response =requests.get("http://api.callectiv.com/subject/12345", headers=headers)
-		# json_response = json.loads(response.content)
-		# self.assertEqual(json_response.get("message"), u"Callactiv Test")
-		# self.assertEqual(json_response.get("reference"), "12345")
-		# self.assertEqual(json_response.get("contact")["phone"], "0207508668")
-		
-
-# class GetConnectionsForSubjectTests(CallectivTestCase):
-# 	def test_connections_default_accept_type(self):
-
-# 		url = "http://api.callectiv.com/subject/12345/connections"
-# 		headers = {'Content-Type':"application/xml", 'Authorization':self.token}
-# 		subject = requests.get(url, headers=headers)
-# 		self.assertEqual(subject.status_code, OK)
-# 		self.assertEqual(etree.fromstring(subject.content).tag, "connections")
-
-	# def test_connections_with_json_accept_type(self):
-
-	# 	url = "http://api.callectiv.com/subject/12345/connections"
-	# 	headers = {'Content-Type':APPL_JSON, 'Authorization':self.token}
-	# 	response = requests.get(url, headers=headers)
-	# 	self.assertEqual(response.status_code, OK)
-	# 	self.assertIsNotNone(response.content)
-	# 	output = json.loads(response.content)
-	# 	self.assertIsNotNone(output)
-		
-	# def test_change_status_with_get_method(self):
-	# 	"""test status change with wrong method """
-
-	# 	url = "http://api.callectiv.com/subject/12345/status/enabled"
-	# 	headers = {'Content-Type':APPL_JSON,"accept":APPL_JSON,'authorization':self.token}
-	# 	response = requests.get(url, headers=headers)
-	# 	self.assertNotEqual(response.status_code, OK)
-
-	# def test_change_status_with_disabled_status(self):
-	# 	url = "http://api.callectiv.com/subject/12345/status/disabled"
-	# 	headers = {'Content-Type':APPL_JSON,"accept":APPL_JSON,'authorization':self.token}
-	# 	response = requests.put(url,headers=headers)
-	# 	self.assertEqual(response.status_code, OK)
-	# 	response = requests.put(url,headers=headers)
-	# 	self.assertIsNot(response.status_code, OK)
-
-
-
-
-	# def test_delete_subject_with_reference(self):
-	# 	""" check status code"""
-	# 	url = "http://api.callectiv.com/subject/12345"
-	# 	headers = {'Authorization':self.token}
-	# 	delete =requests.delete(url, headers= headers)
-	# 	self.assertEqual(delete.status_code, OK)
-		
-	# def test_delete_subject_with_wrong_reference(self):
-	# 	"""check status code"""
-	# 	url = "http://api.callectiv.com/subject/2347"
-	# 	delete = requests.delete(url, headers=self.headers)
-	# 	self.assertIsNot(delete.status_code, OK)
-
-	# def time(self, text):
-	# 	date = parser.parse(text)
-	# 	return date.isoformat()
-
-
-	# def test_make_connections(self):
-	# 	url = "http://api.callectiv.com/connection"
-	# 	headers = {'Content-Type':APPL_JSON,'Authorization':self.token}
-	# 	time = self.time(str(datetime.datetime.now()))
-	# 	request_a = {
-	# 				"from":{
-	# 					"phone":"23327508668",
-	# 					"message":"Hello world"
-	# 				},
-	# 				"subjectReference":"12345",
-	# 				"startDateTime": time}
-
-	# 	request_b = {
-	# 				"to":{
-	# 					"phone":"23320755555",
-	# 					"message":"How are you"
-	# 				},
-	# 				"subjectReference":"12345",
-	# 				"startDateTime":time
-	# 				}
-	# 	request_a = requests.post(url, headers=headers, data=request_a)
-	# 	request_b = requests.post(url, headers=headers, data=request_b)
-	# 	self.assertEqual(request_a.status_code, OK)
-	# 	self.assertEqual(request_b.status_code, OK)
-
-	# def test_get_connection_details(self):
-	# 	"""check the response body and assert if its equal to what was sent in the post request"""
-	# 	self.detail_url = "http://api.callectiv.com/connection/{connectionId}"
-
-	# 	pass
-
-	# def get_connection_status(self)
-
-
-
-
-
+	def time(self, text):
+		date = parser.parse(text)
+		return date.isoformat()
 
 
 if __name__ == "__main__":
 	unittest.main()
 
 
-
-
-
-
-
-
-		# self.connection_request ={"from":{
-		# 							"phone":"233207508668",
-		# 							"message":"Hello world"
-		# 							},
-		# 						"subjectReference":"12345","startDateTime": self.time("Wed, 30 Oct 2013 10:50:05 +0000")}
